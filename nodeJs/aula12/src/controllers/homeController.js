436166a53c17f4e01bc640ae7206de949a3c9b3a
Helen Oliveira <sousahelen816@gmail.com>
2022-08-23 14:43:56 -0300

    create home model
    Criacao de um model com conexao com o mongodb

--- nodeJs/aula12/src/controllers/homeController.js
+++ nodeJs/aula12/src/controllers/homeController.js
@@ -1,3 +1,16 @@
+const HomeModel = require('../models/homeModel')
+
+HomeModel.find()
+.then(dados => console.log(dados))
+.catch(e => console.log(e))
+
+HomeModel.create({
+    titulo: 'title 2',
+    descricao: 'Description 2'
+})
+.then(dados => console.log(dados))
+.catch(e => console.log(e))
+
 exports.paginaInicial = (req, res) => {
         res.render('index')
         return ;
