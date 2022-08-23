c75acdb0101c18c999b1fceafb8ba5109cd653b8
Helen Oliveira <sousahelen816@gmail.com>
2022-08-23 14:31:21 -0300

    conectando com mongoDB

--- nodeJs/aula12/server.js
+++ nodeJs/aula12/server.js
@@ -1,8 +1,19 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
 const routes = require('./routes')
 const path = require('path')
 const {middlewareGlobal} = require('./src/middlewares/middleware')
+const mongoose = require('mongoose')
+
+
+mongoose.connect(process.env.CONECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('conectei a base de dados')
+        app.emit('pronto')
+    })
+    .catch(e => console.log(e))
 
 
 app.use(express.urlencoded ( {extended: true})   )
@@ -20,7 +31,9 @@ app.use(middlewareGlobal)
 app.use(routes)
 
 
-
-app.listen(3000, () => {
-    console.log('servidor rodando na porta 30000')
+app.on('pronto', () => {
+    app.listen(3000, () => {
+        console.log('servidor rodando na porta 30000')
+    })
 })
+
